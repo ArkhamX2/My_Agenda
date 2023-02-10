ddd456b084b28bdd67eb40f77d32fc9def44f348
PolShestogo <99017989+PolShestogo@users.noreply.github.com>
2023-02-10 21:17:53 +0300

    Разные размеры расписаний
    Типо 3 на 2 2 на 3 и 1 на 6

---
+++ MyAgenda/MyAgenda/MVVM/ViewModel/MedSizeMainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyAgenda.Core;
+
+namespace MyAgenda.MVVM.ViewModel
+{
+
+    class MedSizeMainViewModel : ObservableObject
+    {
+        public MidSizeViewModel HomeVM { get; set; }
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MedSizeMainViewModel()
+        {
+            HomeVM = new MidSizeViewModel();
+            CurrentView = HomeVM;
+        }
+    }
+ 
+    
+}
---
+++ MyAgenda/MyAgenda/MVVM/ViewModel/MinSizeMainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyAgenda.Core;
+
+namespace MyAgenda.MVVM.ViewModel
+{
+    class MinSizeMainViewModel : ObservableObject
+    {
+        public MinSizeViewModel HomeVM { get; set; }
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MinSizeMainViewModel()
+        {
+            HomeVM = new MinSizeViewModel();
+            CurrentView = HomeVM;
+        }
+    }
+}

951195497a5fbdd9e0be76fb741832c0bd799665
vadimyt <pvcs@yandex.ru>
2023-02-13 16:42:35 +0300

    Изменение вида таблицы при изменении окна
    +Удаление мусора

--- MyAgenda/MyAgenda/MVVM/ViewModel/MedSizeViewModel.cs
+++ MyAgenda/MyAgenda/MVVM/ViewModel/MedSizeViewModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MyAgenda.MVVM.ViewModel
 {
-    class MidSizeViewModel
+    class MedSizeViewModel
     {
     }
 }
--- MyAgenda/MyAgenda/MainWindow.xaml.cs
+++ MyAgenda/MyAgenda/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyAgenda.MVVM.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,31 @@ namespace MyAgenda
     /// </summary>
     public partial class MainWindow : Window
     {
+        MainViewModel ShowModel = new MainViewModel();
         public MainWindow()
         {
             InitializeComponent();
+
+            SizeChanged += MainWindow_SizeChanged;
+        }
+
+
+        private void MainWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if(MainWind.ActualWidth>1100)
+            {
+                Content.Content = ShowModel.HomeCurrentView;
+            }
+            else
+                if (MainWind.ActualWidth > 700)
+            {
+                Content.Content = ShowModel.MidCurrentView;
+            }    
+            else
+                Content.Content = ShowModel.MinCurrentView;
+
+
+            //throw new NotImplementedException();
         }
     }
 }

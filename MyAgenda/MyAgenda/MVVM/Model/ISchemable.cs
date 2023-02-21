1da451e68a3028e6ab4ce42b989e0d906830c55d
Alexander <106086600+dubstepTractor@users.noreply.github.com>
2023-02-22 00:57:30 +0300

    bizzare interfaces

---
+++ MyAgenda/MyAgenda/MVVM/Model/ISchemable.cs
@@ -0,0 +1,32 @@
+﻿namespace MyAgenda.MVVM.Model
+{
+    /// <summary>
+    /// Интерфейс, позволяющий классу иметь схему таблицы в базе данных
+    /// и уникальный идентификатор.
+    /// </summary>
+    internal interface ISchemable
+    {
+        // UNDONE: Невозможно использовать из-за ограничений .Net Framework.
+        // Обновление до .Net должно решить проблему.
+        // public const string IdColumn = "id";
+
+        // UNDONE: Невозможно использовать из-за ограничений .Net Framework.
+        // Обновление до .Net должно решить проблему.
+        // static IData FromData();
+
+        /// <summary>
+        /// Получить схему таблицы с данными.
+        /// </summary>
+        /// <returns>Схема, заполненная данными.</returns>
+        Schema ToData();
+
+        /// <summary>
+        /// Доступ к идентификатору.
+        /// </summary>
+        int Id
+        {
+            get;
+            set;
+        }
+    }
+}

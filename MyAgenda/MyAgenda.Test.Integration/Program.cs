e0bb5694fa1f71c2ffcfd606ac7a361a0c2d906f
Alexander <106086600+dubstepTractor@users.noreply.github.com>
2023-03-10 20:14:04 +0300

    basic initialization test

--- MyAgenda/MyAgenda.Test.Integration/Program.cs
+++ MyAgenda/MyAgenda.Test.Integration/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MyAgenda.Library;
 
 namespace MyAgenda.Test.Integration
 {
@@ -18,6 +19,16 @@ namespace MyAgenda.Test.Integration
         private static void Main()
         {
             // TODO: Начать разработку тестов интеграции.
+
+            Manager.GetGroup(0);
+            Manager.GetSubject(0);
+            Manager.GetCourse(0);
+            Manager.GetDaySchedule(0);
+            Manager.GetFaculty(0);
+            Manager.GetTeacher(0);
+            Manager.GetWeekType(0);
+            Manager.GetGroupWeekSchedule(0, Manager.GetGroup(0), Manager.GetWeekType(0));
+            Manager.GetTeacherWeekSchedule(Manager.GetTeacher(0), Manager.GetWeekType(0));
         }
     }
 }

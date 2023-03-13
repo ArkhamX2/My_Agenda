8d3310a15170f2e7bc97ef3d9a61fbfb6f5e5194
vadimyt <pvcs@yandex.ru>
2023-03-13 21:08:14 +0300

    Merge Fix

---
+++ MyAgenda/MyAgenda/MVVM/View/Editor/EditWeekDays/ThursdayEditPageView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyAgenda.MVVM.View.Editor.EditWeekDays
+{
+    /// <summary>
+    /// Логика взаимодействия для ThursdayEditPageView.xaml
+    /// </summary>
+    public partial class ThursdayEditPageView : Page
+    {
+        public ThursdayEditPageView()
+        {
+            InitializeComponent();
+        }
+    }
+}
